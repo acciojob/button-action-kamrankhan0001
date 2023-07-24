26b794678fab70071273d2cb11a8a47940c25f0a
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-24 21:45:42 +0530

    submitting code-Mon Jul 24 2023 21:45:40 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  function showParagraph() {
+    const para = document.getElementById('para');
+    para.classList.toggle('hide');
+    para.classList.toggle('show');
+  }
 
   return (
     <div className="App" id="main">
       <p id="para" className="hide">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-      <button id="click">Click Me!</button>
+      <button id="click" onclick="showParagraph()">Click Me</button>
     </div>
   );
 }
