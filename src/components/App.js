f8ff4ebc88c70ab8aa0f05e7e0e1e5ce5c74ab68
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-24 22:49:36 +0530

    submitting code-Mon Jul 24 2023 22:49:34 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-  function showParagraph() {
-    const para = document.getElementById('para');
-    para.classList.toggle('hide');
-    para.classList.toggle('show');
-  }
 
+  function showParagraph() {
+    const paragraph = document.getElementById("para");
+    if (paragraph.classList.contains("hide")) {
+      paragraph.classList.remove("hide");
+      paragraph.classList.add("show");
+    } else {
+      paragraph.classList.remove("show");
+      paragraph.classList.add("hide");
+    }
+  }
+  
   return (
-    <div className="App" id="main">
+    <div id="main">
       <p id="para" className="hide">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
       <button id="click" onclick="showParagraph()">Click Me</button>
     </div>
