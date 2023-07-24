77e7c224870ab9f108ba6bb51684995c91af64c6
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-24 21:30:50 +0530

    submitting code-Mon Jul 24 2023 21:30:49 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const App = (props) => {
   return (
     <div className="App" id="main">
       <p id="para" className="hide">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-      <button id="click" className="show">Click Me!</button>
+      <button id="click">Click Me!</button>
     </div>
   );
 }
