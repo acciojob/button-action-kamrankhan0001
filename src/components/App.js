edd171d933cde31bb017711d2f964c4b61040fe0
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-24 23:15:19 +0530

    submitting code-Mon Jul 24 2023 23:15:17 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-  // State to manage the visibility of the paragraph
-  const [isParagraphVisible, setIsParagraphVisible] = useState(false);
+ 
+  const [show, setShow] = useState(false);
 
-  // Function to toggle the visibility of the paragraph
+ 
   const handleClick = () => {
-    setIsParagraphVisible(!isParagraphVisible);
+    setShow(!show);
   };
 
   return (
     <div id="main">
-      {/* Use the `isParagraphVisible` state to conditionally set the className */}
-      <p id="para" className={isParagraphVisible ? "" : "hide"}>
+      
+      <p id="para" className={show ? "" : "hide"}>
         Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
       </p>
-      {/* Attach the `handleClick` function to the button's onClick event */}
+    
       <button id="click" onClick={handleClick}>Click Me</button>
     </div>
   );
